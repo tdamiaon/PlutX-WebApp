31ab7f76ed9f5faff25f5b6aee1d1807aee11798
Tiago Damião <tdamiaon@gmail.com>
2025-01-26 18:15:16 +0000

    Solved

--- stock_tracker/stock_tracker/settings.py
+++ stock_tracker/stock_tracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6m%1ibqp^m^yr^x0x$e6u#7hg(imts06h_rm7bq--%s^$p&3bg'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
